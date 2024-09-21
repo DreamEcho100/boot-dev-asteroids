7067667505e43238bdcac76f80101d03388edfd5
DreamEcho100 <maze6572198@gmail.com>
2024-09-21 10:11:56 +0300

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
